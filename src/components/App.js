0ab4579f5cef51a1ac3095b401243c18c0c04376
Khushi Jain <khushijain.mp@gmail.com>
2023-08-18 20:19:41 +0530

    connect-4

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Gameboard from "./GameBoard";
+
+
+function App(){
+    return (
+        <div className = "app-container">
+            <Gameboard/>
+        </div>
+    )
+}
+export default App;
